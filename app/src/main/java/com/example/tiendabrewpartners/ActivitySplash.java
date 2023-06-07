85caf5a6bd4d209b6b9fa1bd4c4fab924dbea842
morenoaj <ajmoreno24@outlook.com>
2023-06-07 11:40:02 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/tiendabrewpartners/ActivitySplash.java
@@ -0,0 +1,30 @@
+package com.example.tiendabrewpartners;
+
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivitySplash extends AppCompatActivity {
+
+    ProgressBar splashProgress;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        splashProgress=findViewById(R.id.splashProgress);
+        ObjectAnimator.ofInt(splashProgress,"progress",100).setDuration(6000).start();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(ActivitySplash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },6000);
+    }
+}
