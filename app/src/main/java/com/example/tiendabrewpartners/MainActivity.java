3fcdef172ffb317621442858f1039a692df657bc
Isaac Samaniego <isaac.samaniego@utp.ac.pa>
2023-06-07 17:54:59 -0500

    "Add menu layout: 3"

--- app/src/main/java/com/example/tiendabrewpartners/MainActivity.java
+++ app/src/main/java/com/example/tiendabrewpartners/MainActivity.java
@@ -4,22 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText nombre;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nombre  = findViewById(R.id.etxt_nombre);
     }
 
-    public void menu() {
-        Intent intent=new Intent(this, );
+    public void menu(View view) {
+        Intent intent=new Intent(this, MenuActivity.class);
+        intent.putExtra("nombre", nombre.getText().toString());
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/tiendabrewpartners/MenuActivity.java
+++ app/src/main/java/com/example/tiendabrewpartners/MenuActivity.java
@@ -6,9 +6,11 @@ import android.os.PersistableBundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends MainActivity {
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
     }
+
 }
